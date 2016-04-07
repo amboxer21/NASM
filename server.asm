389a65d42203e38673e3b2832fb4782267dfdf80
Anthony Guevara <amboxer21@gmail.com>
2016-04-07 15:10:57 -0400

    Rearranged constants.

--- server.asm
+++ server.asm
@@ -2,18 +2,19 @@ section .data
   start: db 'Starting server.',10
   start_len: equ $-start
 
-  ip:   equ '0x7F000001' ;127.0.0.1
-  port: equ '1030'
-
   SYS_EXIT:  equ 1
   SYS_WRITE: equ 4
   SYS_SEND:  equ 9
   AF_INET:   equ 2
   SYS_RECV:  equ 10
   SYS_SOKET: equ 1
+
   SYS_CONNECT:    equ 3
   SOCK_STREAM:    equ 1
   SYS_SOCKETCALL: equ 102
+
+  port: equ '1030'
+  ip:   equ '0x7F000001' ;127.0.0.1
 
 section .text
 
