63a4ea619d1f52f0ca79928f5af39f8584e09fad
Anthony Guevara <amboxer21@gmail.com>
2016-04-10 20:31:28 -0400

    Small changes like storage size for the ip address.

--- server.asm
+++ server.asm
@@ -15,7 +15,7 @@ section .data
   SYS_SOCKETCALL: equ 102
 
   port: equ '1030'
-  ip:   equ '0x7F000001' ;127.0.0.1
+  ip:   dq '0x7F000001' ;127.0.0.1
 
 section .text
 
@@ -38,9 +38,9 @@ section .text
       sub  esp, 12
 
       ;socket
-      mov  [ebp -  4], AF_INET
-      mov  [ebp -  8], SOCK_STREAM
-      mov  [ebp - 12], 0
+      mov  [ebp -  4], dword AF_INET
+      mov  [ebp -  8], dword SOCK_STREAM
+      mov  [ebp - 12], dword 0
 
       ;bind
 
