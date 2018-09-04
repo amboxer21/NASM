d910ad708c181df11044f14d9690f42bbc452ea5
amboxer21 <amboxer21@gmail.com>
2018-09-04 15:38:07 -0400

    Updated server.asm

--- server.asm
+++ server.asm
@@ -1,67 +1,81 @@
+bits 32
+global main
 section .data
 
-  start: db 'Starting server.',10
-  start_len: equ $-start
+    start: db 'Starting server.',10
+    start_len: equ $-start
 
-  SYS_EXIT:  equ 1
-  SYS_WRITE: equ 4
-  SYS_SEND:  equ 9
-  AF_INET:   equ 2
-  SYS_RECV:  equ 10
-  SYS_SOKET: equ 1
+    SYS_BIND:   equ 49
 
-  SOCK_PROTO:     equ 0
-  SYS_CONNECT:    equ 3
-  SOCK_STREAM:    equ 1
-  SYS_SOCKETCALL: equ 102
+    SYS_EXIT:   equ 1
+    SYS_WRITE:  equ 4
+    SYS_SEND:   equ 9
+    AF_INET:    equ 2
+    SYS_RECV:   equ 10
+    SYS_SOCKET: equ 1
 
-  port: equ '1030'
-  ip:   dq '0x7F000001' ;127.0.0.1
+    SOCK_PROTO:     equ 6
+    SYS_CONNECT:    equ 3
+    SOCK_STREAM:    equ 1
+    SYS_SOCKETCALL: equ 102
+
+section .bss
+    socket_address: resd 2
+    port: word 0x8813 ;port 5000
+    ip: dword 0x00000000 ;ip 0.0.0.0
 
 section .text
-    global _start
 
-        ;'starting server' message
-        :starting_server_message
-            mov eax, 4
-            mov ebx, 1
-            mov ecx, start
-            mov edx, start_len
-            int 80h
+    ;starting server message
+    starting_server_message:
+        mov edx, start_len
+        mov ecx, start
+        mov ebx, 1
+        mov eax, 4
+        int 80h
 
-        ;void exit(int status)
-        :exit
-            mov eax, SYS_EXIT
-            mov ebx, 0 ;Exit code of 0(No error).
-            int 80h
+    ;void exit(int status)
+    exit:
+        mov ebx, 0 ;Exit code of 0(No error).
+        mov eax, SYS_EXIT
+        int 80h
 
-        ;int socket(int family, int type, int protocol)
-        :socket
-            mov  [ebp -  4], dword AF_INET
-            mov  [ebp -  8], dword SOCK_STREAM
-            mov  [ebp - 12], dword SOCK_PROTO
-            mov  [ebp - 14], dword SYS_SOCKETCALL
-            int 80h
-            ret
+    ;int socket(int family, int type, int protocol)
+    socket:
+        mov [ebp -  4], dword AF_INET
+        mov [ebp -  8], dword SOCK_STREAM
+        mov [ebp - 12], dword SOCK_PROTO
+        mov [ebp - 14], dword SYS_SOCKETCALL
+        mov eax, SYS_SOCKET
+        int 80h
+        ret
 
-        ;bind
-
-
-        ;accept
-
-
-        ;loop
-
-
-        ;receive
+    ;int bind(int fd, struct sockaddr * sockaddr, int addrlen)
+    bind:
+        
+        mov eax, SYS_BIND
+        int 80h
+        ret
 
 
-        ;send
+    ;accept
 
 
-        ;closefd
+    ;loop
 
-        _start:
+
+    ;receive
+
+
+    ;send
+
+
+    ;closefd
+
+    main:
+
+        call starting_server_message
+        call socket
 
         ;push ebp
         ;mov  ebp, esp
